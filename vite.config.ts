29a7cd37f4a8fd9b04da53e2123a8a2442d25bd3
Prashul Jain <jainprashul@gmail.com>
2024-11-21 17:54:04 +0530

    initialize project with React, TypeScript, and Vite setup

---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.ts',
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Transact',
+      short_name: 'Transact',
+      description: 'An Expense Recording app.',
+      theme_color: '#1f1f1f',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+
+  server: {
+    port: 3000,
+    open: true,
+  },
+
+  resolve: {
+    alias: {
+      "@" : path.resolve(__dirname, "src"),
+    }
+  }
+})
